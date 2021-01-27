c96ff5280feb274064c1f57eb85bd74d738fe238
lpofredc <frederic.cloitre@lpo.fr>
2021-01-27 15:14:21 +0100

    Array exos

--- exos/2-3_arrays/index.js
+++ exos/2-3_arrays/index.js
@@ -22,27 +22,57 @@ document.addEventListener(
           - l'afficher dans la console
           - afficher son texte dans la console
         */
-
+        console.log('--- reponse 1 ---', tweets[0].full_text)
         console.log('--- Question 2 ---');
         // afficher dans la console les textes de tous les tweets du tableau en utilisant .forEach()
-
+        tweets.forEach((element, index) => {
+          console.log(index, element.full_text)
+        });
         console.log('--- Question 3 ---');
         // créer un tableau avec seulement tous les textes des tweets en utilisant .map()
-
+        const tweet_texts = tweets.map(e => {
+          return e.full_text
+        })
+        console.log('only texts', tweet_texts)
         console.log('--- Question 4 ---');
         // créer un tableau avec seulement les dates de publication
-
+        const tweets_createdate = tweets.map(e => {
+          return e.created_at
+        })
+        console.log('create dates', tweets_createdate)
         console.log('--- Question 5 ---');
         // créer un tableau avec seulement les tweets en français en utilisant .filter()
-
+        const tweetsfr = tweets.filter(e => {
+          return e.lang === 'fr'
+        })
+        console.log('only french', tweetsfr)
         console.log('--- Question 6 ---');
         // trouver le tweet qui parle de 'Moonlighter' en utilisant .find()
+        const tweets_moonlighter = tweets.find(e => {
+          return e.full_text.includes('Moonlighter')
+        })
+        console.log('Moonlighter', tweets_moonlighter)
+
+
 
         console.log('--- Question 7 ---');
         // lister tous les textes de hashtags du premier tweet
+        const a_hashtags = tweets[0].entities.hashtags.map(e => {
+          return e.text
+        })
+        console.log('first hashtags', a_hashtags)
+
 
         console.log('--- BONUS ---');
         // lister les 56 tweets qui ont pour hashtag `coronadiary`
+
+        const coronadiary = tweets.filter((e, i, a) => {
+          // return e.entities.hashtags.includes('coronadiary')
+          return e.entities.hashtags.find(e => {
+            return e.text === 'coronadiary'
+          })
+        })
+        console.log(coronadiary)
       })
       .catch(function (e) {
         console.error(e);
